ad1dd650a03109108dc875834b4bfaebceb18837
WillRichey1234 <gsampson@vms.edu>
2017-09-20 11:45:08 -0600

    Creating Arrays Part2

---
+++ CreatingArraysPart2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CreatingArraysPart2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+           string zig = "You can get what you want our of your life " +
+                "if you help enough other people get what they want out of theirs";
+
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
+            // here we are converting the string into characters so that we can revers every character
+            // using array.reverse so that it will print the message backwards
+            foreach (char zigChar in charArray)
+                // for each allows this code to be ran for each of the things in the array
+                Console.Write(zigChar);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
